5a4b24c1db2c7647aaf1058502def89b574ed513
orlovdevloper1-png <orlovdevloper.1@gmail.com>
2026-03-06 17:09:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const path = require("path");
 
-app.use(express.static("public"))
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req,res)=>{
-res.sendFile(__dirname+"/public/index.html")
-})
+// Static files (HTML CSS JS)
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000,()=>{
-console.log("PromoVerse Running")
-})
+// Home Page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Advertise page example
+app.get("/advertise", (req, res) => {
+  res.send("<h1>Advertise your Discord Server here 🚀</h1>");
+});
+
+// Search page example
+app.get("/search", (req, res) => {
+  res.send("<h1>Search Discord Servers 🔎</h1>");
+});
+
+// API example
+app.get("/api/servers", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "PromoVerse API Running"
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`PromoVerse running on port ${PORT}`);
+});
