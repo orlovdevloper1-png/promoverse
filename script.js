16886b0e238c072a3a2a58c79437d35b753e30bf
orlovdevloper1-png <orlovdevloper.1@gmail.com>
2026-03-06 16:53:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("PromoVerse Website Loaded");
+function showForm(){
+document.getElementById("server-form").classList.toggle("hidden")
+}
+
+function addServer(){
+
+let name=document.getElementById("name").value
+let link=document.getElementById("link").value
+
+let server=document.createElement("div")
+server.className="server"
+
+server.innerHTML=`
+<h3>${name}</h3>
+<a href="${link}" target="_blank">Join Server</a>
+`
+
+document.getElementById("server-list").appendChild(server)
+
+}
